94f88a9fabe49d940962c237f6492d6eda915308
RenGeng <geng2608@hotmail.com>
2018-05-19 19:32:54 +0200

    rien

--- test.c
+++ test.c
@@ -6,12 +6,15 @@ int main(int argc, char const *argv[])
 {
 	double A[8] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0};
 	double B[8] = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
+	double d[8] = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
 	double C[8];
-	__m256d a,b,c;
+	__m256d a,b,c,d;
 
 	a = _mm256_load_pd(&A[0]);
 	b = _mm256_load_pd(&B[0]);
+	d = _mm256_load_pd(&D[0]);
 	c = _mm256_add_pd(a,b);
+	c = _mm256_add_pd(c,d):
 
 	_mm256_store_pd(&C[0],c);
 
